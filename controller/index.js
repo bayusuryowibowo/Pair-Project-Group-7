cf7e3451c3f35eea37a1d789726ed918ff5c01ce
bayusuryowibowo <bayusuryowibowo@gmail.com>
2023-07-07 00:52:30 +0700

    login dgn password kosong BUG-FIXED

--- controller/index.js
+++ controller/index.js
@@ -8,7 +8,7 @@ class Controller {
     const role = req.session.role
     Dish.findAll()
       .then((dishes) => {
-        res.render('Home', { dishes,formatCurrency,role })
+        res.render('Home', { dishes, role })
       })
       .catch((err) => {
         console.log(err);
@@ -34,9 +34,9 @@ class Controller {
   }
 
 
-  static login (req,res) {
-    let {error} = req.query;
-    res.render("Login",{error})
+  static login(req, res) {
+    let { error } = req.query;
+    res.render("Login", { error })
 
   }
 
@@ -44,111 +44,106 @@ class Controller {
     const { username, password } = req.body
     if (!username) {
       let error = `Username is required`;
-
-      res.redirect(`/login?error=${error}`)
-
+      return res.redirect(`/login?error=${error}`)
     }
     if (!password) {
       let error = `Password is required`;
-
-      res.redirect(`/login?error=${error}`)
-
+      return res.redirect(`/login?error=${error}`)
     }
     User.findOne({
       where: {
         username: username
       }
     })
-
-    .then((user)=>{
-    const isCorrectPassword = bcrpyt.compareSync(password, user.password)
-    if (isCorrectPassword){
-      req.session.userId = user.id;//set userId ke session 
-      req.session.role = user.role;//set role ke session
-      return res.redirect('/')
-    } else {
-      let error = 'Invalid username/password'
-      return res.redirect(`/login?error=${error}`)
-    }
-    })
-    .catch((err)=>{
-      console.log(err);
-      res.send(err);
-    })
+      .then((user) => {
+        const isCorrectPassword = bcrpyt.compareSync(password, user.password)
+        if (isCorrectPassword) {
+          req.session.userId = user.id;//set userId ke session 
+          req.session.role = user.role;//set role ke session
+          return res.redirect('/')
+        } else {
+          let error = 'Invalid username/password'
+          return res.redirect(`/login?error=${error}`)
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      })
 
   }
 
   static dishes(req, res) {
     Dish.findAll()
-    .then((dataDish)=>{
-      res.render("Dishes", {dataDish,formatCurrency})
-    })
-    .catch ((err)=>{
-      console.log(err);
-      res.send(err);
-    })
+      .then((dataDish) => {
+        res.render("Dishes", { dataDish, formatCurrency })
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      })
   }
 
-   static logout (req,res) {
-    req.session.destroy((err)=>{
-      if(err){
+  static logout(req, res) {
+    req.session.destroy((err) => {
+      if (err) {
         console.log(err);
         res.send(err)
       } else res.redirect('/')
     })
   }
-  
-  
-  static reservation (req,res) {
+
+
+  static reservation(req, res) {
     const msg = req.query.msg
     Dish.findAll()
-    .then((dataDish)=>{
-      res.render("Reservation", {dataDish,msg})
-    })
-    .catch ((err)=>{
-      console.log(err);
-      res.send(err);
-    })
+      .then((dataDish) => {
+        res.render("Reservation", { dataDish, msg })
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      })
   }
 
-   static postReservation(req,res) {
-    const {date, tableNumber,DishId} = req.body
+  static postReservation(req, res) {
+    const { date, tableNumber, DishId } = req.body
     const UserId = req.session.userId
     let totalPrice
     Dish.findOne({
-      where:{
-        id:DishId
+      where: {
+        id: DishId
       }
     })
-    .then((data)=>{
-      totalPrice = data.price;
-      console.log(date,tableNumber,DishId,totalPrice)
-    return Reservation.create({date,tableNumber,totalPrice,DishId,UserId})
-    })
-    .then(()=>{
-      res.redirect('/reservation?msg=ReservationAdded')
-    })
-    .catch((err)=>{
-      console.log(err);
-      res.send(err);
-    })
+      .then((data) => {
+        totalPrice = data.price;
+        console.log(date, tableNumber, DishId, totalPrice)
+        return Reservation.create({ date, tableNumber, totalPrice, DishId, UserId })
+      })
+      .then(() => {
+        res.redirect('/reservation?msg=ReservationAdded')
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      })
 
   }
 
-  static cancelReservation(req,res) {
+  static cancelReservation(req, res) {
     const UserId = req.session.userId
-    Reservation.findAll({where:{UserId:UserId}})
-    .then((data)=>{
-      res.render('CancelReservation',{data,formatCurrency,})
-    })
-    .catch((err)=>{
-      console.log(err);
-      res.send(err);
-    })
+    Reservation.findAll({ where: { UserId: UserId } })
+      .then((data) => {
+        res.render('CancelReservation', { data, formatCurrency, })
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      })
 
   }
 
-  static dataReservation(req,res) {
+  static dataReservation(req, res) {
     res.render('secretReservation')
 
 
